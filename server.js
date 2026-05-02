fb259165a51acfa8258f761112e313e0cf7f644d
sahilsharma2246 <sahilwsharma2246@gmail.com>
2026-05-02 21:38:26 +0530

    updated new files

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/resumeDB")
-  .then(() => console.log("MongoDB-Connected"))
+  .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
 app.use("/api/resume", resumeRoutes);
